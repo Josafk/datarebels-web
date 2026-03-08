aae936d2d3d2be6052c76ab2bd81d919d9b463d7
Josafk <josafat@datarebels.mx>
2026-03-08 17:23:35 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
+      { 
+        protocol: "https", 
+        hostname: "images.unsplash.com", 
+        pathname: "/**" 
+      },
     ],
   },
 };
