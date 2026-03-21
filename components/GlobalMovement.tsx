e473d3297438dcf4cad35b209bd7fcde78a73810
Josafk <josafat@datarebels.mx>
2026-03-21 15:46:51 -0600

    fix: force svg size 24px inside icon container

--- components/GlobalMovement.tsx
+++ components/GlobalMovement.tsx
@@ -71,7 +71,7 @@ export function GlobalMovement() {
                 key={benefit.title}
                 className="flex gap-4 items-start"
               >
-                <div className="flex-shrink-0 w-8 h-8 bg-[#1330F4] rounded-lg p-1.5 flex items-center justify-center shrink-0">
+                <div className="flex-shrink-0 w-10 h-10 bg-[#1330F4] rounded-lg p-2 flex items-center justify-center shrink-0 [&>svg]:w-6 [&>svg]:h-6 [&>svg]:flex-shrink-0">
                   <benefit.icon className="w-5 h-5 text-white" />
                 </div>
                 <div className="flex flex-col gap-1">
